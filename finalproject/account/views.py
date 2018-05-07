44f2a9811838e37c090341c55fee67ea04d5e69b
Kevin <kevin77836@gmail.com>
2018-05-07 11:07:05 +0800

    update

---
+++ finalproject/account/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, render_to_response
+
+def index(request):
+	return render_to_response("account/index.html", locals())
+
+def login(request):
+	return render_to_response("account/ntu_shopping.html", locals())
